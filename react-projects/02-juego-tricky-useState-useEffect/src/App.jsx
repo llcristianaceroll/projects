aa0c8bf246c0edeca1c652b90010e624ce4af990
Cristian Acero <llcristianaceroll@gmail.com>
2024-03-29 17:29:44 -0500

    creating the board game

---
+++ react-projects/02-juego-tricky-useState-useEffect/src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+
+
+const TURNS = {
+  X : 'x',
+  O : 'o'
+}
+
+const board = Array(9).fill(null)
+function App() {
+  return (
+    <main className='board'>
+    <h1>juego</h1>
+       <section  className='game'>
+       {
+        board.map((_,index) => {
+          return (
+            <div className='cell' key={index}>
+            <span className='cell_content'>
+              {index}
+            </span>
+            </div>
+          )
+        })
+       }
+
+       </section>
+    </main>
+  )
+
+}
+
+export default App
