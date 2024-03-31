a1df20357244a065acc3abba4ec682c27f492121
Cristian Acero <llcristianaceroll@gmail.com>
2024-03-31 16:07:03 -0500

    setting new board

--- react-projects/02-juego-tricky-useState-useEffect/src/App.jsx
+++ react-projects/02-juego-tricky-useState-useEffect/src/App.jsx
@@ -1,44 +1,58 @@
-
-import { useState } from 'react';
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
 
 const TURNS = {
-  X : 'x',
-  O : 'o'
-}
+  X: "x",
+  O: "o",
+};
 
-const Square = ({children, upgradeBoard, index}) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
   return (
-      <div className='square'>
-        {children}
-      </div>
-  )
-}
-// const board = Array(9).fill(null)
+    <div className={className} onClick={handleClick}>
+      {children}
+    </div>
+  );
+};
 
 function App() {
+  
+  const [board, setBoard] = useState(Array(9).fill(null));
 
-  const [board, setBoard] = useState(['x', 'x', 'x','x', 'x', 'x','o', 'x', 'x']);
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
 
   return (
-    <main className='board'>
-    <h1>juego</h1>
-       <section  className='game'>
-       {
-        board.map((_, index) => {
+    <main className="board">
+      <h1>juego</h1>
+      <section className="game">
+        {board.map((_, index) => {
           return (
-          <Square key={index} index={index}>
-            {board[index]}
-          </Square>
-          )
-        })
-       }
-
-       </section>
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
     </main>
-  )
-
+  );
 }
 
-export default App
+export default App;
